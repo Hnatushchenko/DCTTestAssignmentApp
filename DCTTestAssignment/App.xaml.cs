7fc1871dcdbadd399025cbc8a20d05c22b76a53a
Hnatushchenko <gnatushenko.andrij@gmail.com>
2023-01-24 10:45:27 +0200

    install Caliburn Micro and create MVVM structure

--- DCTTestAssignment/App.xaml.cs
+++ DCTTestAssignment/App.xaml.cs
@@ -8,10 +8,19 @@ using System.Windows;
 
 namespace DCTTestAssignment
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        private readonly Bootstrapper _bootstrapper;
+
+        public App()
+        {
+            _bootstrapper = new Bootstrapper();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            
+            base.OnStartup(e);
+        }
     }
 }
