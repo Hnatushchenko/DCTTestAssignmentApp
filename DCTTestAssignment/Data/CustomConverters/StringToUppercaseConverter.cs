edc5a57fabed73db3d2c7afa270144228afc074e
Hnatushchenko <gnatushenko.andrij@gmail.com>
2023-01-24 19:33:13 +0200

    create custom StringToUppercase converter

---
+++ DCTTestAssignment/Data/CustomConverters/StringToUppercaseConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace DCTTestAssignment.Data.CustomConverters;
+
+public class StringToUppercaseConverter : IValueConverter
+{
+    public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is string stringValue)
+        {
+            return stringValue.ToUpper();
+        }
+        return value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
