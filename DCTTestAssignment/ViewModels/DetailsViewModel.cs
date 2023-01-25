1d4635d5666e0d42e55eff0a5d91562f7671d934
Hnatushchenko <gnatushenko.andrij@gmail.com>
2023-01-25 12:07:37 +0200

    create DetailsViewModel

---
+++ DCTTestAssignment/ViewModels/DetailsViewModel.cs
@@ -0,0 +1,40 @@
+﻿using Caliburn.Micro;
+using CoinGecko;
+using CoinGecko.Responces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DCTTestAssignment.ViewModels;
+
+public class DetailsViewModel : Screen
+{
+    private readonly string _cryptocurrencyId;
+    private readonly ICoinGeckoApiClient _coinGeckoApiClient;
+
+    public DetailsViewModel(string cryptocurrencyId, ICoinGeckoApiClient coinGeckoApiClient)
+    {
+        _cryptocurrencyId = cryptocurrencyId;
+        _coinGeckoApiClient = coinGeckoApiClient;
+    }
+
+    protected override async Task OnActivateAsync(CancellationToken cancellationToken)
+    {
+        CoinFullData = await _coinGeckoApiClient.GetCoinFullDataAsync(_cryptocurrencyId);
+    }
+
+    private CoinFullData? _coinFullData;
+
+    public CoinFullData? CoinFullData
+    {
+        get { return _coinFullData; }
+        set
+        {
+            _coinFullData = value;
+            NotifyOfPropertyChange(() => _coinFullData);
+        }
+    }
+}
