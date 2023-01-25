9725e43cc34af5dd3e8be9f5a1c95fc6ea769062
Hnatushchenko <gnatushenko.andrij@gmail.com>
2023-01-25 16:43:04 +0200

    create custom converter for dictionary values binding

---
+++ DCTTestAssignment/Data/CustomConverters/DictionaryValuesBindingConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace DCTTestAssignment.Data.CustomConverters;
+
+public class DictionaryValuesBindingConverter : IValueConverter
+{
+    public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is Dictionary<string, decimal?> dictionary)
+        {
+            if (parameter is string key)
+            {
+                return dictionary[key];
+            }
+        }
+        return "not converted";
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
