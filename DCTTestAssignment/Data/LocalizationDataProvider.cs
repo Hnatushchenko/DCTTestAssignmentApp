a31474ee5db028cee755688e747040b10ab818ab
Hnatushchenko <gnatushenko.andrij@gmail.com>
2023-01-28 12:09:59 +0200

    improve performence of LocalizationDataProvider by using reflection only on startup and caching the results

--- DCTTestAssignment/Data/LocalizationDataProvider.cs
+++ DCTTestAssignment/Data/LocalizationDataProvider.cs
@@ -1,27 +1,42 @@
-﻿using System;
+﻿using DCTTestAssignment.Data.LocalizationData.DetailsViewLocalizationData;
+using DCTTestAssignment.Data.ThemeSupport.ThemeData;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls.Primitives;
 
 namespace DCTTestAssignment.Data;
 
 public class LocalizationDataProvider<TLocalizationData> : ILocalizationDataProvider<TLocalizationData>
 {
+    private Dictionary<string, TLocalizationData> _viewLocalizationData = new Dictionary<string, TLocalizationData>();
+    public LocalizationDataProvider()
+    {
+        var localizationDataTypes = Assembly.GetExecutingAssembly().GetTypes()
+            .Where(type => typeof(TLocalizationData).IsAssignableFrom(type))
+            .Where(type => !type.IsAbstract && !type.IsInterface);
+
+        var localizationDataInstances = localizationDataTypes.Select(Activator.CreateInstance).Cast<TLocalizationData>();
+            
+        foreach (var localizationData in localizationDataInstances)
+        {
+            if (localizationData is null) continue;
+            
+            var localizationDataName = localizationData.GetType().Name.Replace("LocalizationData", string.Empty);
+            localizationDataName = localizationDataName.Remove(0, count: localizationDataName.IndexOf("View") + "View".Length);
+            _viewLocalizationData.Add(localizationDataName, localizationData);
+        }
+    }
     public TLocalizationData GetLocalizationData(string language)
     {
-        var targetType = GetType().Assembly.GetTypes()
-            .Where(type => type.IsClass)
-            .Where(type => type.Name.EndsWith($"{language}LocalizationData"))
-            .Where(type => type.IsAssignableTo(typeof(TLocalizationData))).Single();
-
-        try
-        {
-            return (TLocalizationData)Activator.CreateInstance(targetType);
-        }
-        catch (InvalidCastException)
+        if (_viewLocalizationData.ContainsKey(language) == false)
         {
             throw new NotSupportedException($"{language} language is not supported.");
         }
+
+        return _viewLocalizationData[language];
     }
 }
