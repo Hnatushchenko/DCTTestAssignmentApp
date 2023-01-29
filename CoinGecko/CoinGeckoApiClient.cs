54cfc85f7a93a96e5935a5f2f91bd9fff1ec7c08
Hnatushchenko <gnatushenko.andrij@gmail.com>
2023-01-29 09:07:03 +0200

    refactor CoinGeckoApiClient

--- CoinGecko/CoinGeckoApiClient.cs
+++ CoinGecko/CoinGeckoApiClient.cs
@@ -2,6 +2,7 @@
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Json;
@@ -10,101 +11,81 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Web;
 
-namespace CoinGecko
+namespace CoinGecko;
+
+public class CoinGeckoApiClient : ICoinGeckoApiClient
 {
-    public class CoinGeckoApiClient : ICoinGeckoApiClient
+    private readonly HttpClient _httpClient;
+    public CoinGeckoApiClient()
     {
-        private readonly HttpClient _httpClient;
-        public CoinGeckoApiClient()
+        var socketHttpHandler = new SocketsHttpHandler()
         {
-            var socketHttpHandler = new SocketsHttpHandler()
-            {
-                PooledConnectionLifetime = TimeSpan.FromMinutes(1)
-            };
-            _httpClient = new HttpClient(socketHttpHandler)
-            {
-                BaseAddress = ApiEndPoints.BaseApiEndPoint
-            };
+            PooledConnectionLifetime = TimeSpan.FromMinutes(1)
+        };
+        _httpClient = new HttpClient(socketHttpHandler)
+        {
+            BaseAddress = ApiEndPoints.BaseApiEndPoint
+        };
+    }
+
+    private async Task<TData> GetDataAsync<TData>(string uri)
+    {
+        var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, uri);
+        var response = await _httpClient.SendAsync(httpRequestMessage)
+            .ConfigureAwait(false);
+
+        response.EnsureSuccessStatusCode();
+
+        var responseContent = await response.Content.ReadAsStringAsync();
+
+        try
+        {
+            return JsonConvert.DeserializeObject<TData>(responseContent)
+                ?? throw new ArgumentNullException(nameof(responseContent));
+        }
+        catch (Exception e)
+        {
+            throw new HttpRequestException(e.Message);
+        }
+    }
+
+    public async Task<IReadOnlyCollection<CoinsMarkets>> GetCoinsMarketsAsync(string currency = "usd")
+    {
+        var uri = ApiEndPoints.CoinsMarkets + $"?vs_currency={currency}&order=market_cap_desc&per_page=100&page=1&sparkline=false";
+        return await GetDataAsync<IReadOnlyCollection<CoinsMarkets>>(uri);
+    }
+
+    public async Task<CoinFullData> GetCoinFullDataAsync(string id)
+    {
+        var uri = ApiEndPoints.AllDataByCoinId(id) + "?localization=false";
+        return await GetDataAsync<CoinFullData>(uri);
+    }
+
+    public async Task<IReadOnlyCollection<CoinsListItem>> GetCoinListAsync()
+    {
+        return await GetDataAsync<IReadOnlyCollection<CoinsListItem>>(ApiEndPoints.CoinsList);
+    }
+
+    public async Task<decimal?> GetCoinPriceInUSDByNameOrSymbol(string identifier)
+    {
+        var coinList = await GetCoinListAsync();
+        var coin = coinList.FirstOrDefault(coin =>
+            coin.Name?.ToLower() == identifier.ToLower());
+
+        if (coin is null)
+        {
+            coin = coinList.FirstOrDefault(coin =>
+            coin.Symbol?.ToLower() == identifier.ToLower());
         }
 
-        public async Task<IReadOnlyCollection<CoinsMarkets>> GetCoinsMarketsAsync(string currency = "usd")
+        if (coin is not null && coin.Id is not null)
         {
-            var response = await _httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Get, ApiEndPoints.CoinsMarkets + $"?vs_currency={currency}&order=market_cap_desc&per_page=100&page=1&sparkline=false"))
-                .ConfigureAwait(false);
-
-            response.EnsureSuccessStatusCode();
-
-            var responseContent = await response.Content.ReadAsStringAsync();
-
-            var coinsMarketsList = JsonConvert.DeserializeObject<IReadOnlyCollection<CoinsMarkets>>(responseContent);
-
-            if (coinsMarketsList is null)
-            {
-                throw new HttpRequestException("Responce is empty");
-            }
-
-            return coinsMarketsList;
+            var coinFullData = await GetCoinFullDataAsync(coin.Id);
+            decimal price = coinFullData.MarketData.CurrentPrice["usd"] 
+                ?? throw new HttpRequestException("Price was not provided by the CoinGecko API");
+            
+            return price;
         }
-
-        public async Task<CoinFullData> GetCoinFullDataAsync(string id)
-        {
-            var response = await _httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Get, ApiEndPoints.AllDataByCoinId(id) + "?localization=false"))
-                .ConfigureAwait(false);
-
-            response.EnsureSuccessStatusCode();
-
-            var responseContent = await response.Content.ReadAsStringAsync();
-
-            var coinFullData = JsonConvert.DeserializeObject<CoinFullData>(responseContent);
-
-            if (coinFullData is null)
-            {
-                throw new HttpRequestException("Responce is empty");
-            }
-
-            return coinFullData;
-        }
-
-        public async Task<IReadOnlyCollection<CoinsListItem>> GetCoinListAsync()
-        {
-            var response = await _httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Get, ApiEndPoints.CoinsList))
-                .ConfigureAwait(false);
-
-            response.EnsureSuccessStatusCode();
-
-            var responseContent = await response.Content.ReadAsStringAsync();
-
-            var coinFullData = JsonConvert.DeserializeObject<IReadOnlyCollection<CoinsListItem>>(responseContent);
-
-            if (coinFullData is null)
-            {
-                throw new HttpRequestException("Responce is empty");
-            }
-
-            return coinFullData;
-        }
-
-        public async Task<decimal?> GetCoinPriceInUSDByNameOrSymbol(string identifier)
-        {
-            var coinList = await GetCoinListAsync();
-            var coin = coinList.FirstOrDefault(coin =>
-                coin.Name?.ToLower() == identifier.ToLower());
-
-            if (coin is null)
-            {
-                coin = coinList.FirstOrDefault(coin =>
-                coin.Symbol?.ToLower() == identifier.ToLower());
-            }
-
-            if (coin is not null && coin.Id is not null)
-            {
-                var coinFullData = await GetCoinFullDataAsync(coin.Id);
-                decimal price = coinFullData.MarketData.CurrentPrice["usd"] 
-                    ?? throw new HttpRequestException("Price was not provided by the CoinGecko API");
-                
-                return price;
-            }
-            return null;
-        }
+        return null;
     }
 }
